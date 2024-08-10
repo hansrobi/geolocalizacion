d5b3c193159ef3c293075b45f865ee59c6a4d039
Hans <hansvd90@gmail.com>
2024-08-09 22:50:09 -0500

    agregando ips mongodb...

--- server/index.js
+++ server/index.js
@@ -20,10 +20,7 @@ const corsOptions = {
 
 // Conectar a MongoDB
 mongoose
-    .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
+    .connect(process.env.MONGO_URI)
     .then(() => console.log("Conectado a MongoDB Atlas"))
     .catch((err) => console.error("Error al conectar a MongoDB Atlas", err));
 
