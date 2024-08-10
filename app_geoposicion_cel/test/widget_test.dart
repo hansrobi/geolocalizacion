c630dd906d3dd6f9bd37483e7b9fbf3a95b5cce3
Hans <hansvd90@gmail.com>
2024-08-10 15:49:21 -0500

    estilos css modificados

--- app_geoposicion_cel/test/widget_test.dart
+++ app_geoposicion_cel/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:app_geoposicion_cel/main.dart';
+import 'package:GeoAmigo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
